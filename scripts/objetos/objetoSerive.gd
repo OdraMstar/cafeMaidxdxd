81a6be2b6c9400667a09404534326475db250b22
odra <odraaiart@gmail.com>
2024-05-29 22:49:50 -0700

    se corige commit

---
+++ scripts/objetos/objetoSerive.gd
@@ -0,0 +1,17 @@
+extends RigidBody3D
+class_name  ObjetoServible
+
+@export var nombre : String
+var esta_ocupado : bool
+@onready var colision = $CollisionShape3D
+
+func seAgarro():
+	esta_ocupado = true
+	colision.disabled = true
+	pass
+
+func seSolto():
+	esta_ocupado = false
+	colision.disabled = false
+	pass
+
